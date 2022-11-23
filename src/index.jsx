2a92b3ea0363f2c36921ca0e72c8a26ae0fa9d51
ange0 <kouassiangey@gmail.com>
2022-11-23 23:55:27 +0000

    fix bug

--- src/index.jsx
+++ src/index.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Route, Routes, } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './index.css';
 import Home from './Pages/Home';
 import Shopping from './Pages/Shopping';
+import reportWebVitals from './reportWebVitals';
 
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Router>
-      <Routes>
-        <Route exact path="/">
+        <Route exact path='/'>
           <Home />
         </Route>
-        <Route path="/shopping">
+        <Route path='/shopping'>
           <Shopping />
         </Route>
-      </Routes>
     </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
